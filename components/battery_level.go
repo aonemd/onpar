7d8627d3881a167010c843ec43d9961c49226f06
aonemd <aonemdsaleh@gmail.com>
2018-01-28 16:24:28 +0200

    Add icons to battery_level component

--- components/battery_level.go
+++ components/battery_level.go
@@ -1,25 +1,52 @@
+// requires `acpi`
+
 package main
 
 import (
 	"fmt"
 	"os/exec"
 	"regexp"
+	"strconv"
 )
 
 const (
-	battery = "0"
+	battery        = "0"
+	critical_level = 10
+
+	charging_icon      = ""
+	full_icon          = " "
+	three_quarter_icon = ""
+	half_icon          = ""
+	empty_icon         = ""
 )
 
 func main() {
 	response, _ := exec.Command("acpi", "-b", " | ", "grep", "Battery ", battery).Output()
 
-	// statePattern, _ := regexp.Compile("Full|Charging|Discharging")
+	iconPattern, _ := regexp.Compile("Full|Charging|Discharging")
 	powerPattern, _ := regexp.Compile("[0-9]+%")
 	remainingTimePattern, _ := regexp.Compile("[01][0-9]:[0-9][0-9]")
 
-	// state := statePattern.FindString(string(response))
-	power := powerPattern.FindString(string(response))
+	icon := iconPattern.FindString(string(response))
+	power, _ := strconv.Atoi(powerPattern.FindString(string(response)))
 	remainingTime := remainingTimePattern.FindString(string(response))
 
-	fmt.Println(fmt.Sprintf("BAT: %s (%s)", power, remainingTime))
+	switch icon {
+	case "Full":
+		icon = full_icon
+	case "Charging":
+		icon = charging_icon
+	case "Discharging":
+		if power <= critical_level {
+			icon = empty_icon
+		} else if power <= 50 {
+			icon = half_icon
+		} else if power <= 75 {
+			icon = three_quarter_icon
+		} else {
+			icon = full_icon
+		}
+	}
+
+	fmt.Println(fmt.Sprintf("%s %v (%s)", icon, power, remainingTime))
 }
