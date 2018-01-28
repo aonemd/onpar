2b3540138ec6614f9f1071d9c09b0dfff7085061
aonemd <aonemdsaleh@gmail.com>
2018-01-28 15:05:05 +0200

    Add icons to DarkSky weather forecast

--- components/dark_sky_weather.go
+++ components/dark_sky_weather.go
@@ -11,6 +11,19 @@ const (
 	api_key = "api_key"
 	lon_lat = "lon,lat"
 	units   = "si"
+
+	clear_day_icon           = ""
+	clear_night_icon         = ""
+	partly_cloudy_day_icon   = ""
+	partly_cloudy_night_icon = ""
+	cloudy_icon              = ""
+	rain_icon                = ""
+	sleet_icon               = ""
+	snow_icon                = ""
+	wind_icon                = ""
+	fog_icon                 = ""
+	celsius_icon             = "°C"
+	fahrenheit_icon          = "°F"
 )
 
 func main() {
@@ -24,7 +37,35 @@ func main() {
 	currentForcast := forecastMap["currently"].(map[string]interface{})
 
 	summary := currentForcast["summary"]
+	icon := currentForcast["icon"]
 	temperature := currentForcast["temperature"]
 
-	fmt.Println(fmt.Sprintf("%s %v°C", summary, temperature))
+	switch icon {
+	case "clear-day":
+		icon = clear_day_icon
+	case "clear-night":
+		icon = clear_night_icon
+	case "partly-cloudy-day":
+		icon = partly_cloudy_day_icon
+	case "partly-cloudy-night":
+		icon = partly_cloudy_night_icon
+	case "cloudy":
+		icon = cloudy_icon
+	case "rain":
+		icon = rain_icon
+	case "sleet":
+		icon = sleet_icon
+	case "snow":
+		icon = snow_icon
+	case "wind":
+		icon = wind_icon
+	case "fog":
+		icon = fog_icon
+	}
+
+	if units == "si" {
+		fmt.Println(fmt.Sprintf("%s  %s %v %s", icon, summary, temperature, celsius_icon))
+	} else {
+		fmt.Println(fmt.Sprintf("%s  %s %v %s", icon, summary, temperature, fahrenheit_icon))
+	}
 }
