82910022d1a535327931986189b0fe95bf9687c3
aonemd <aonemdsaleh@gmail.com>
2020-06-30 21:39:34 +0200

    Add general music component based on `playerctl`

---
+++ components/music_component.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	// "regexp"
+	"strings"
+)
+
+func main() {
+	var _response []byte
+	var _err error
+
+	_response, _err = exec.Command("playerctl", "metadata", "artist").Output()
+	if _err != nil {
+		return
+	}
+	artist := strings.TrimSpace(string(_response))
+
+	_response, _err = exec.Command("playerctl", "metadata", "title").Output()
+	if _err != nil {
+		return
+	}
+	title := strings.TrimSpace(string(_response))
+
+	if artist != "" && title != "" {
+		fmt.Println(fmt.Sprintf(" %s - %s", artist, title))
+	}
+}
