69b3e1f1152be9d49608c16afb491ee33bceaf0e
aonemd <aonemdsaleh@gmail.com>
2020-07-04 18:50:51 +0200

    Add a few improvements
    - Read artist and title from `playerctl`'s metadata command directly. It
      seems to output the most updated status
    - Remove artist if the player is `chrome`

--- components/music_component.go
+++ components/music_component.go
@@ -1,28 +1,55 @@
+// requires `playerctl`
+
 package main
 
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
+var (
+	status         []string
+	filteredStatus []string
+	_response      []byte
+	_err           error
+	player         string
+	artist         string
+	title          string
+)
+
 func main() {
-	var _response []byte
-	var _err error
-
-	_response, _err = exec.Command("playerctl", "metadata", "artist").Output()
+	_response, _err = exec.Command("playerctl", "metadata").Output()
 	if _err != nil {
 		return
 	}
-	artist := strings.TrimSpace(string(_response))
 
-	_response, _err = exec.Command("playerctl", "metadata", "title").Output()
-	if _err != nil {
-		return
-	}
-	title := strings.TrimSpace(string(_response))
+	playerPattern, _ := regexp.Compile("^([\\w]+)")
+	player = playerPattern.FindString(string(_response))
 
-	if artist != "" && title != "" {
-		fmt.Println(fmt.Sprintf(" %s - %s", artist, title))
+	artistPattern, _ := regexp.Compile(":artist\\s+(.*)")
+	artistFoundPattern := artistPattern.FindStringSubmatch(string(_response))
+	if artistFoundPattern != nil && player != "chrome" {
+		artist = strings.TrimSpace(artistFoundPattern[1])
 	}
+
+	titlePattern, _ := regexp.Compile(":title\\s+(.*)")
+	titleFoundPattern := titlePattern.FindStringSubmatch(string(_response))
+	if titleFoundPattern != nil {
+		title = strings.TrimSpace(titleFoundPattern[1])
+	}
+
+	status = []string{
+		artist,
+		title,
+	}
+
+	for _, stat := range status {
+		if stat != "" {
+			filteredStatus = append(filteredStatus, stat)
+		}
+	}
+
+	fmt.Println(fmt.Sprintf(" %s", strings.Join(filteredStatus, " - ")))
 }
